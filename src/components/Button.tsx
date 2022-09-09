665966e339abcfd77441a1322787bc78ef85fd3c
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-09-08 20:02:38 -0500

    Add pill attr to paypal-button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,15 +5,22 @@ import paypalLogoWhite from "../assets/paypal-logo-white.svg";
 
 type Props = {
   color: "gold" | "blue" | "silver" | "white" | "black";
+  shape: "rect" | "pill";
 };
 
-function Button({ color }: Props) {
+function Button({ color, shape }: Props) {
   const theme = {
-    gold: { bg: "#F6C657", pp: ppLogoBlue, paypal: paypalLogoBlue },
-    blue: { bg: "#459AD8", pp: ppLogoWhite, paypal: paypalLogoWhite },
-    silver: { bg: "#EEE", pp: ppLogoBlue, paypal: paypalLogoBlue },
-    white: { bg: "#FFF", pp: ppLogoBlue, paypal: paypalLogoBlue },
-    black: { bg: "#2C2E2F", pp: ppLogoWhite, paypal: paypalLogoWhite },
+    color: {
+      gold: { bg: "#F6C657", pp: ppLogoBlue, paypal: paypalLogoBlue },
+      blue: { bg: "#459AD8", pp: ppLogoWhite, paypal: paypalLogoWhite },
+      silver: { bg: "#EEE", pp: ppLogoBlue, paypal: paypalLogoBlue },
+      white: { bg: "#FFF", pp: ppLogoBlue, paypal: paypalLogoBlue },
+      black: { bg: "#2C2E2F", pp: ppLogoWhite, paypal: paypalLogoWhite },
+    },
+    shape: {
+      rect: { radius: "4px" },
+      pill: { radius: "18px" },
+    },
   };
 
   return (
@@ -29,12 +36,12 @@ function Button({ color }: Props) {
         overflow: "hidden",
         transition: "opacity 0.25s ease-in-out",
         height: "35px",
-        "border-radius": "4px",
-        "background-color": theme[color].bg,
+        "border-radius": theme.shape[shape].radius,
+        "background-color": theme.color[color].bg,
       }}
     >
-      <img src={theme[color].pp} />
-      <img src={theme[color].paypal} />
+      <img src={theme.color[color].pp} />
+      <img src={theme.color[color].paypal} />
     </button>
   );
 }
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { customElement } from "solid-element";
 import Button from "./components/Button";
 
 function registerWebComponents() {
-  customElement("paypal-button", { color: "gold" }, Button);
+  customElement("paypal-button", { color: "gold", shape: "rect" }, Button);
 }
 
 registerWebComponents();
